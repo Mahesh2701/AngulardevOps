78b67d90ef596cadf7e34c5fb088e0a71c9ff63a
Mahesh <koturwar27@gmail.com>
2022-02-02 21:01:07 +0530

    added new project UI

---
+++ DockerFile
@@ -0,0 +1,2 @@
+FROM nginx
+COPY ./dist/UI /usr/share/nginx/html
---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  constructor(private http: HttpClient){}
+  ngOnInit(): void {
+   alert('On Init');
+    this.http.get('http://localhost:5005/weatherforecast').subscribe(
+      (result) =>{
+        result ?alert('hello'): alert('false');
+      });
+
+      this.http.get('http://localhost:5004/weatherforecast').subscribe(
+      (result) =>{
+        result ?alert('hello222'): alert('false2222');
+      });
+  }
+  title = 'UI';
+}
