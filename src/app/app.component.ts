05fa7313df73e32f6c9fa00f8d821992b03d96e2
Mahesh <koturwar27@gmail.com>
2022-02-28 19:07:19 +0530

    added docker hub

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,7 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
    alert('On Init');
    alert("container");
+   alert('dockerhub updated images');
     this.http.get('http://3.110.223.63:5001/weatherforecast').subscribe(
       (result) =>{
         result ?alert('hello'): alert('false');
